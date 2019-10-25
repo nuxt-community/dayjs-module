9719a1792e7356b3e26e39b1beeba30516f6a1ca
Takuma HANATANI <mail@potato4d.me>
2019-10-26 02:51:43 +0900

    feat(types): Enhance context types

--- lib/module.d.ts
+++ lib/module.d.ts
@@ -1,6 +1,25 @@
 import Vue from 'vue'
 import dayjs from 'dayjs'
 
+declare module '@nuxt/vue-app' {
+  interface Context {
+    $dayjs: typeof dayjs
+  }
+  interface NuxtAppOptions {
+    $dayjs: typeof dayjs
+  }
+}
+
+// Nuxt 2.9+
+declare module '@nuxt/types' {
+  interface Context {
+    $dayjs: typeof dayjs
+  }
+  interface NuxtAppOptions {
+    $dayjs: typeof dayjs
+  }
+}
+
 declare module 'vue/types/vue' {
   interface Vue {
     $dayjs: typeof dayjs
