a824046ed9f56be43fca8ba0013af3308baf5310
Bryan Velastegui <bvelastegui@intisana.com>
2020-08-13 23:35:01 -0500

    ✨ Add plugins support Added tests for advancedFormat and localized plugins

--- test/system/module.spec.ts
+++ test/system/module.spec.ts
@@ -62,4 +62,18 @@ describe('basic', () => {
     const newValue = await newTextContent.jsonValue()
     return expect(newValue).toBe('1995/12/18(月曜日)')
   })
+
+  test('support plugins', async () => {
+    expect.assertions(2)
+    await page.goto(url('/plugin'))
+    const el = await page.$('[data-test-id="advancedFormat"]')
+    const textContent = await el!.getProperty('textContent')
+    const value = await textContent.jsonValue()
+    expect(value).toBe('16th')
+
+    const el1 = await page.$('[data-test-id="localized"]')
+    const textContent1 = await el1!.getProperty('textContent')
+    const value1 = await textContent1.jsonValue()
+    return expect(value1).toBe('Aug 16, 2018')
+  })
 })
