9457db39396f0bd2f732d65b81184931cd7d45ae
Nathan Chase <zampa_@hotmail.com>
2022-07-25 18:54:27 -0400

    initial commit of Nuxt 3 branch

---
+++ playground/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Example: Date formatting</h1>
+    <p> Unformatted: {{ new Date() }}</p>
+    <p> Formatted with dayjs (template): {{ $dayjs(new Date()).format('DD/MM/YYYY') }}</p>
+    <p> Formatted with dayjs (computed): {{ prettyDate }}</p>
+    <p> Formatted with dayjs (RelativeTime): {{ $dayjs(new Date()).fromNow() }}</p>
+  </div>
+</template>
+
+<script setup>
+const nuxtApp = useNuxtApp()
+const prettyDate = computed(() => nuxtApp.$dayjs(new Date()).format('dddd, MMMM D, YYYY h:mm A'))
+</script>
--- test/unit/module.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('module.ts', () => {
-  describe('sample', () => {
-    it('should returns true', () => {
-      expect(true).toBe(true)
-    })
-  })
-})
